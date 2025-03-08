f73b807cdc2dbdda8f63b1c6537677b184a80658
ronik297 <ronik.allen@gmail.com>
2025-03-08 22:19:39 +0530

    added signup route

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,43 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
 app.post("/api/v1/user/signup", async (c) => {
+  const body = await c.req.json();
+
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  return c.text("Hello Hono!");
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        name: body.name,
+        password: body.password,
+      },
+    });
+
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET
+    );
+
+    return c.text(jwt);
+  } catch (error) {
+    console.log("error", error);
+    return c.text("Invalid");
+  }
 });
 
 app.post("/api/v1/user/signin", (c) => {
