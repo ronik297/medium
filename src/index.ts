372902eae2b0be599fdc78a0faf9cd8c8de17915
ronik297 <ronik.allen@gmail.com>
2025-03-09 22:26:05 +0530

    added userRouter and blogRouter, authMiddleware, and completed all routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { decode, sign, verify } from "hono/jwt";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
 
 const app = new Hono<{
   Bindings: {
@@ -10,54 +9,7 @@ const app = new Hono<{
   };
 }>();
 
-app.post("/api/v1/user/signup", async (c) => {
-  const body = await c.req.json();
-
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  try {
-    const user = await prisma.user.create({
-      data: {
-        username: body.username,
-        name: body.name,
-        password: body.password,
-      },
-    });
-
-    const jwt = await sign(
-      {
-        id: user.id,
-      },
-      c.env.JWT_SECRET
-    );
-
-    return c.text(jwt);
-  } catch (error) {
-    console.log("error", error);
-    return c.text("Invalid");
-  }
-});
-
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.get("/api/v1/blog/bulk", (c) => {
-  return c.text("Hello Hono!");
-});
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 export default app;
---
+++ src/routes/user.ts
@@ -0,0 +1,79 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const body = await c.req.json();
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        name: body.name,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.text("Invalid");
+    }
+
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET
+    );
+
+    return c.text(jwt);
+  } catch (error) {
+    console.log("error", error);
+    return c.text("Invalid");
+  }
+});
+
+userRouter.post("/signin", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: body.username,
+        password: body.password,
+      },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.text("Invalid credentials");
+    }
+
+    const jwt = await sign(
+      {
+        id: user.id,
+      },
+      c.env.JWT_SECRET
+    );
+
+    return c.text(jwt);
+  } catch (error) {
+    console.log("error", error);
+    c.status(411);
+    return c.text("Invalid credentials");
+  }
+});
