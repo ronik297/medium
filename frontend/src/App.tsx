5cf5b363b6d41023391675c924f174ca33741757
ronik297 <ronik.allen@gmail.com>
2025-03-12 17:00:16 +0530

    added / route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,8 +10,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Signin />} />
           <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/publish" element={<Publish />} />
