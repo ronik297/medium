e164ecffa6561b83cc124b7e12d6b8a1b230c555
ronik297 <ronik.allen@gmail.com>
2025-04-22 23:02:39 +0530

    added update user profile settings

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -142,3 +142,56 @@ userRouter.get("/me", async (c) => {
     return c.text("Error fetching user data:" + error);
   }
 });
+
+userRouter.put("/setting", async (c) => {
+  const authHeader = c.req.header("authorization");
+  const isAuthenticated = await verify(authHeader || "", c.env.JWT_SECRET);
+
+  if (!isAuthenticated) {
+    c.status(403);
+    return c.text("Invalid credentials");
+  }
+
+  const body = await c.req.json();
+  const { success } = signupInput.safeParse(body);
+
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "All fields are required",
+    });
+  }
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      username: body.username,
+    },
+  });
+
+  if (existingUser && existingUser.id !== Number(isAuthenticated.id)) {
+    c.status(403);
+    return c.text("Username already exists");
+  }
+
+  try {
+    // TODO: Hash the password before saving it to the database
+    const user = await prisma.user.update({
+      where: {
+        id: Number(isAuthenticated.id),
+      },
+      data: {
+        username: body.username,
+        name: body.name,
+        password: body.password,
+      },
+    });
+
+    return c.json({ user, message: "User data updated successfully" });
+  } catch (error) {
+    c.text("Error updating user data:" + error);
+  }
+});
