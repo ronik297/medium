f36486539234e666d0410eb6ee91f067e20a38b5
ronik297 <ronik.allen@gmail.com>
2025-04-28 02:18:55 +0530

    removed import

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -5,7 +5,7 @@ import { Spinner } from "../components/Spinner";
 import { Save } from "lucide-react";
 
 export default function Settings() {
-  const { user, error, loading } = useUser();
+  const { user, loading } = useUser();
 
   const [userSettings, setUserSettings] = useState({
     username: "",
